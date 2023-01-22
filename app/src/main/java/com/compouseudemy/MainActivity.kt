a4976dbb306f056337c18f8faaf263c43b378b02
luis <luis.hev1920@gmail.com>
2023-01-21 21:05:24 -0600

    Base app

---
+++ app/src/main/java/com/compouseudemy/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.compouseudemy
+
+import android.media.Image
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+           NewStory()
+        }
+    }
+}
+
+@Composable
+private fun NewStory(){
+    val image = painterResource(id = R.mipmap.header)
+
+    Column (modifier = Modifier.padding(16.dp)){
+        Image(painter = image, contentDescription = "Header")
+        Text(text = "Titulo de la noticia")
+        Text(text = "Titulo de la noticia")
+        Text(text = "Titulo de la noticia")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNewStory(){
+    NewStory()
+}
